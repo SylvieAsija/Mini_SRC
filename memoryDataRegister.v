bc3515bac460fbc6dc95466e9f5f4c3afe15d0e4
group117 <group117@elec377.queensu.ca>
2024-02-01 10:40:03 -0500

    started MDR

--- memoryDataRegister.v
+++ memoryDataRegister.v
@@ -1,4 +1,4 @@
-module memoryDataRegister(input [31:0] BusMuxOut, Mdatain, output [31:0] out, input read, clear, clock, MDRin)
-    
+module memoryDataRegister(input [31:0] BusMuxOut, Mdatain, output [31:0] BusMuxIn-MDR, memChip, input read, clear, clock, MDRin)
+
 
 endmodule
