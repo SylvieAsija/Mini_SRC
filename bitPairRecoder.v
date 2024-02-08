8418c1b16c085ec68e44ba1fbafe894d9967592e
group117 <group117@elec377.queensu.ca>
2024-02-08 11:05:41 -0500

    git progress on bit pair recoding, not done

--- bitPairRecoder.v
+++ bitPairRecoder.v
@@ -1,12 +1,17 @@
-module bitPairRecoder(input [2:0] bitsToConvert, output [2:0] recodedOut)
+module bitPairRecoder(input [31:0] inMultiplier, input [2:0] bitsToConvert, output [32:0] recodedOut)
 
 always @ (*)
     case (bitsToConvert)
-        3'b000, 3'b111: recodedOut [1:0] = 'b000 //0
-        3'b001, 3'b010: recodedOut [1:0] = 'b001 //+1
-        3'b011        : recodedOut [1:0] = 'b010 //+2
-        3'b100        : recodedOut [1:0] = 'b110 //-2
-        3'b101, 3'b110: recodedOut [1:0] = 'b101 //-1
+        // 3'b000, 3'b111: recodedOut [1:0] = 'b000 //0
+        // 3'b001, 3'b010: recodedOut [1:0] = 'b001 //+1
+        // 3'b011        : recodedOut [1:0] = 'b010 //+2
+        // 3'b100        : recodedOut [1:0] = 'b110 //-2
+        // 3'b101, 3'b110: recodedOut [1:0] = 'b101 //-1
+        3'b000, 3'b111: recodedOut [32:0] = 33'b0
+        3'b001, 3'b010: recodedOut [32:0] = {2'b0, inMultiplier [31:0]}
+        3'b011        : recodedOut [32:0] = {1'b0, inMultiplier [31:0], 1'b0}
+        3'b100        : recodedOut [32:0] = {1'b1, inMultiplier [31:0], 1'b0} 
+        3'b101, 3'b110: recodedOut [32:0] = {1'b01} // will call negate operation on these once made
         default: recodedOut [1:0] = 'b000;
     endcase
 endmodule
--- multiplier.v
+++ multiplier.v
@@ -1,14 +1,21 @@
-module multiplier(input [31:0] inX, input [31:0] inY, output [31:0] outProduct)
+module multiplier(input [31:0] inX, input [31:0] inY, output [31:0] outProductHigh, output [31:0] outProductLow)
     
-    wire [2:0] signedBitPair
+    wire [32:0] bitPairMultiplier
     wire [32:0] extendInput
-    integer i
+    wire [32:0] tempVal
+    wire [64:0] tempSum
+    wire [32:0] tempMult
+    wire [0] tempSumSignBit
 
+    bitPairMultiplier [32:0] = 33'b0 //initialize bitPairMultiplier
+
+    integer i
     extendInput [32:0] = {inY [31:0], 1'b0} //make initial bit 0 for booth
 
-    for (i = 1, i>31, i = i + 2) begin
-        bitPairRecoder(.bitsToConvert(.extendInput [i+1:i-1]), .recodedOut(.signedBitPair))
-        
+    for (i = 1, i>31, i = i + 2) begin //loop through 
+        tempVal [32:0] = bitPairMultiplier [32:0]
+        bitPairRecoder(.inMultiplier(.inX), .bitsToConvert(.extendInput [i+1:i-1]), .recodedOut(.bitPairMultiplier))
+        carryRippleAdder(.a(.tempVal), .b(.bitPairMultiplier), .cin(1'b0), .cout(.tempSum), .s(.tempSumSignBit)) 
     end
 
 
