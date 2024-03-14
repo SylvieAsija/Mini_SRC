d4fd83682b6ca5cad1d65ef0938e105043eb75e5
Beck Kehoe <beckkehoe@gmail.com>
2024-03-14 11:15:17 -0400

    Add files via upload

--- RAM.v
+++ RAM.v
@@ -1,10 +1,10 @@
-module RAM (input read, write, clock, input [8:0] address, input [31:0] ramDataIn, output [31:0] ramDataOut)
-    reg [31:0] memory [0:511]
+module RAM (input read, write, clock, input [8:0] address, input [31:0] ramDataIn, output reg [31:0] ramDataOut);
+    reg [31:0] memory [0:511];
 
     always @ (posedge clock) begin //some places online write on posedge clock, and also have a reset, used generic for now
         if (write == 1)
             memory[address] <= ramDataIn;
-        if (read == 1)
-            ramDataOut <= memory[address]
+        else if (read == 1)
+            ramDataOut <= memory[address];
     end
 endmodule
