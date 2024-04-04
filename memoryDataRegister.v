29c1a8f8b6469f33699414716e332e80c79cbd9b
EladDallal <98134155+EladDallal@users.noreply.github.com>
2024-04-04 09:40:53 -0400

    memoryDataRegister.v

--- memoryDataRegister.v
+++ memoryDataRegister.v
@@ -1,17 +1,8 @@
-module memoryDataRegister (input clk, clr, MDRen, MDROut, read, input [31:0] Mdatain,input [31:0] busMuxout,
+module memoryDataRegister (input clk, clr, MDRen, read, input [31:0] Mdatain,input [31:0] busMuxout,
 	output wire [31:0] q
 );
 wire [31:0] d;
 
-if(MDRen) begin
-	d = Mdatain
-	register r1(clr, clk, MDRen, d, q);
-end
-else if (MDROut) begin
-	d = busMuxout
-	register r(clr, clk, MDROut, d, q);
-end
-
-//assign d = read ? Mdatain : busMuxout;
-//register r(clr, clk, MDRen, d, q);
+assign d = read ? Mdatain : busMuxout;
+register r(clr, clk, MDRen, d, q);
 endmodule
